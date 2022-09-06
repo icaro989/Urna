37de4785194cd5e5c15fbd5f7374f11069134816
Icaro Fernandes Dias <icaro.dias@fieb.org.br>
2022-09-06 16:15:53 -0300

    Create etapas.js

---
+++ etapas.js
@@ -0,0 +1,38 @@
+let etapas = [{
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [{
+                numero: 38111,
+                nome: 'Ciclano',
+                partido: 'ABC',
+                fotos: [{ url: 'https://img.estadao.com.br/fotos/politica/eleicoes-2020/BA/FBA50000758918_div.jpg', legenda: 'Vereador' }]
+            },
+            {
+                numero: 39222,
+                nome: 'Beltrano',
+                partido: 'CBA',
+                fotos: [{ url: 'https://pbs.twimg.com/profile_images/1399936252179267585/tmTkmbJG_400x400.jpg', legenda: 'Vereador' }]
+
+            }
+        ]
+    },
+    {
+        titulo: 'Prefeito',
+        numeros: 2,
+        candidatos: [{
+                numero: 38,
+                nome: 'Ficano',
+                partido: 'ABC',
+                fotos: [{ url: 'https://img.estadao.com.br/fotos/politica/eleicoes-2020/BA/FBA50000758918_div.jpg', legenda: 'Vereador' }]
+            },
+            {
+                numero: 39,
+                nome: 'Melano',
+                partido: 'CBA',
+                fotos: [{ url: 'https://pbs.twimg.com/profile_images/1399936252179267585/tmTkmbJG_400x400.jpg', legenda: 'Vereador' }]
+
+            }
+        ]
+    }
+
+]
