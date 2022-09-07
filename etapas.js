d83cc8d37b80f5d91a5f579cbead944bc8fd6b53
Icaro Dias <icarodias20@hotmail.com>
2022-09-07 13:45:59 -0300

    Update etapas.js

--- etapas.js
+++ etapas.js
@@ -23,13 +23,13 @@ let etapas = [{
                 numero: '38',
                 nome: 'Ficano',
                 partido: 'ABC',
-                fotos: [{ url: 'https://img.estadao.com.br/fotos/politica/eleicoes-2020/BA/FBA50000758918_div.jpg', legenda: 'Vereador' }]
+                fotos: [{ url: 'https://imgix.prensa.li/https%3A%2F%2Fstatic.prensa.li%2Fstories%2Fd1%2F1d%2Fef%2Fd8%2Fd11defd8-0906-4e71-b9b6-0acf223fcea2.jpg?fit=crop&max-h=450&mh=450&w=800&s=5e0f4aac13ecee4130edacfc352be62f', legenda: 'Prefeito' }]
             },
             {
                 numero: '39',
                 nome: 'Melano',
                 partido: 'CBA',
-                fotos: [{ url: 'https://pbs.twimg.com/profile_images/1399936252179267585/tmTkmbJG_400x400.jpg', legenda: 'Vereador' }]
+                fotos: [{ url: 'https://pbs.twimg.com/profile_images/712174405577785344/gDoubJp1_400x400.jpg', legenda: 'Prefeito' }]
 
             }
         ]
